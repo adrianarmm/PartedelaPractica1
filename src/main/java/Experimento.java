6864db662a10101e670e84343aca90fb55512153
adrianarmm <areyemor@myuax.com>
2024-04-24 16:00:13 +0200

    7

--- src/main/java/Experimento.java
+++ src/main/java/Experimento.java
@@ -1,19 +1,38 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Experimento {
     private List<CultivoDeBacterias> cultivoDeBacteriasList;
 
+    // Constructor que inicializa la lista de cultivos
     public Experimento() {
         this.cultivoDeBacteriasList = new ArrayList<>();
     }
 
+    // Método para agregar un cultivo de bacterias al experimento
     public void agregarCultivoDeBacterias(CultivoDeBacterias cultivoDeBacterias) {
-        cultivoDeBacteriasList.add(cultivoDeBacterias);
+        if (cultivoDeBacterias != null) {
+            cultivoDeBacteriasList.add(cultivoDeBacterias);
+        } else {
+            System.out.println("No se puede agregar un cultivo nulo.");
+        }
     }
 
+    // Método para mostrar todos los cultivos de bacterias
     public void mostrarCultivosDeBacterias() {
-        cultivoDeBacteriasList.forEach(System.out::println);
+        if (cultivoDeBacteriasList.isEmpty()) {
+            System.out.println("No hay cultivos de bacterias para mostrar.");
+        } else {
+            cultivoDeBacteriasList.forEach(System.out::println);
+        }
+    }
+
+    // Método para obtener los nombres de todos los cultivos de bacterias
+    public List<String> obtenerNombresDeCultivos() {
+        return cultivoDeBacteriasList.stream()
+                .map(CultivoDeBacterias::getNombre)
+                .collect(Collectors.toList());
     }
 
     public static void main(String[] args) {
@@ -21,6 +40,8 @@ public class Experimento {
         experimento.agregarCultivoDeBacterias(new CultivoDeBacterias("Bacteria 1", 50));
         experimento.agregarCultivoDeBacterias(new CultivoDeBacterias("Bacteria 2", 20));
         experimento.agregarCultivoDeBacterias(new CultivoDeBacterias("Bacteria 3", 70));
+
         experimento.mostrarCultivosDeBacterias();
+        System.out.println("Nombres de todos los cultivos: " + experimento.obtenerNombresDeCultivos());
     }
 }
