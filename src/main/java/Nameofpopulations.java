4892cf20097bea0d396b967f58ac0f6454a1846c
vitaliy-pg <aljndrvt@myuax.com>
2024-04-25 16:03:30 +0200

    12

--- src/main/java/Nameofpopulations.java
+++ src/main/java/Nameofpopulations.java
@@ -4,6 +4,14 @@ import java.util.List;
 
 public class Nameofpopulations {
 
+    private String nombre;
+    private String fechaInicio;
+    private String fechaFin;
+    private int numBacteriasIniciales;
+    private String temperatura;
+    private String luminosidad;
+    private int[] dosisComida;
+
 
 
 }
