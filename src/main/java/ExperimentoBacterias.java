6d87262ce7271b8016856ff46ef847b18992f63a
vitaliy-pg <aljndrvt@myuax.com>
2024-04-25 16:14:39 +0200

    25

--- src/main/java/ExperimentoBacterias.java
+++ src/main/java/ExperimentoBacterias.java
@@ -1,3 +1,5 @@
+
+import java.util.Random;
 public class ExperimentoBacterias {
 
     public static int crecimientoBacterias(int poblacionInicial, double tasaCrecimiento) {
@@ -7,3 +9,7 @@ public class ExperimentoBacterias {
         nuevaPoblacion += (nuevaPoblacion * variacion) / 100;
         return nuevaPoblacion;
 }
+    public static int eliminarBacterias(int poblacion, double porcentajeEliminacion) {
+        int eliminadas = (int) (poblacion * porcentajeEliminacion);
+        return poblacion - eliminadas;
+    }
