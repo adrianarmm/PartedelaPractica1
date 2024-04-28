b681389947fa85ed3c7ba8d1f3d4acc3c977e3b6
adrianarmm <areyemor@myuax.com>
2024-04-28 11:43:41 +0200

    43

--- src/main/java/EXPERIMENTO/PoblacionBacterias.java
+++ src/main/java/EXPERIMENTO/PoblacionBacterias.java
@@ -10,11 +10,11 @@ class PoblacionBacterias {
     private String fechaFin;
     private int numBacteriasIniciales;
     private String temperatura;
+
     private String luminosidad;
     private int[] dosisComida;
 
-    public PoblacionBacterias(String nombre, String fechaInicio, String fechaFin, int numBacteriasIniciales,
-                              String temperatura, String luminosidad, int[] dosisComida) {
+    public PoblacionBacterias(String nombre, String fechaInicio, String fechaFin, int numBacteriasIniciales, String temperatura, String luminosidad, int[] dosisComida) {
         this.nombre = nombre;
         this.fechaInicio = fechaInicio;
         this.fechaFin = fechaFin;
@@ -24,45 +24,81 @@ class PoblacionBacterias {
         this.dosisComida = dosisComida;
     }
 
-    @Override
-    public String toString() {
-        return "Nombre: " + nombre + "\n" +
-                "Fecha de inicio: " + fechaInicio + "\n" +
-                "Fecha de fin: " + fechaFin + "\n" +
-                "Número de bacterias iniciales: " + numBacteriasIniciales + "\n" +
-                "Temperatura: " + temperatura + "\n" +
-                "Luminosidad: " + luminosidad + "\n" +
-                "Dosis de comida: " + Arrays.toString(dosisComida);
-    }
-}
-
-public class Experimentos {
-    private List<PoblacionBacterias> poblacionesBacterias;
-
-    public Experimentos() {
-        poblacionesBacterias = new ArrayList<>();
+    public String getNombre() {
+        return nombre;
     }
 
-    public void agregarPoblacionBacterias(PoblacionBacterias poblacion) {
-        poblacionesBacterias.add(poblacion);
+    public String getFechaInicio() {
+        return fechaInicio;
     }
 
-    public List<String> nombresPoblacionesBacterias() {
-        List<String> nombres = new ArrayList<>();
-        for (PoblacionBacterias poblacion : poblacionesBacterias) {
-            nombres.add(poblacion.toString());
-        }
-        return nombres;
+    public String getFechaFin() {
+        return fechaFin;
+    }
+
+    public int getNumBacteriasIniciales() {
+        return numBacteriasIniciales;
+    }
+
+    public String getTemperatura() {
+        return temperatura;
+    }
+
+    public String getLuminosidad() {
+        return luminosidad;
+    }
+
+    public int[] getDosisComida() {
+        return dosisComida;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setFechaInicio(String fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public void setFechaFin(String fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public void setNumBacteriasIniciales(int numBacteriasIniciales) {
+        this.numBacteriasIniciales = numBacteriasIniciales;
+    }
+
+    public void setTemperatura(String temperatura) {
+        this.temperatura = temperatura;
+    }
+
+    public void setLuminosidad(String luminosidad) {
+        this.luminosidad = luminosidad;
+    }
+
+    public void setDosisComida(int[] dosisComida) {
+        this.dosisComida = dosisComida;
     }
 
     public static void main(String[] args) {
-        experimentos experimentos = new experimentos();
-        PoblacionBacterias poblacion1 = new PoblacionBacterias("Población 1", "01/01/2021", "01/02/2021", 1000, "25°C", "Alta", new int[]{10, 20, 30});
-        PoblacionBacterias poblacion2 = new PoblacionBacterias("Población 2", "01/02/2021", "01/03/2021", 2000, "30°C", "Baja", new int[]{20, 30, 40});
-        PoblacionBacterias poblacion3 = new PoblacionBacterias("Población 3", "01/03/2021", "01/04/2021", 3000, "35°C", "Media", new int[]{30, 40, 50});
-        experimentos.agregarPoblacionBacterias(poblacion1);
-        experimentos.agregarPoblacionBacterias(poblacion2);
-        experimentos.agregarPoblacionBacterias(poblacion3);
-        experimentos.nombresPoblacionesBacterias().forEach(System.out::println);
+        int[] dosisComida = {10, 20, 30, 40, 50};
+        PoblacionBacterias poblacionBacterias = new PoblacionBacterias("Bacteria 1", "01/01/2021", "01/02/2021", 100, "25°C", "Alta", dosisComida);
+        System.out.println(poblacionBacterias);
     }
+
+    @Override
+
+    public String toString() {
+        return "PoblacionBacterias{" +
+                "nombre='" + nombre + '\'' +
+                ", fechaInicio='" + fechaInicio + '\'' +
+                ", fechaFin='" + fechaFin + '\'' +
+                ", numBacteriasIniciales=" + numBacteriasIniciales +
+                ", temperatura='" + temperatura + '\'' +
+                ", luminosidad='" + luminosidad + '\'' +
+                ", dosisComida=" + Arrays.toString(dosisComida) +
+                '}';
+    }
+
 }
+
