c14d5945f97c970b3bab8596adb386b9276e8ce0
Gmongtor <gmongtor@myuax.com>
2024-05-01 18:09:39 +0200

    55

--- src/main/java/Guardar/FuncionGuardar.java
+++ src/main/java/Guardar/FuncionGuardar.java
@@ -12,6 +12,24 @@ public class GuardarArchivos extends JFrame {
         initializeComponents();
         setUpFrame();
     }
+    private void initializeComponents() {
+        textArea = new JTextArea();
+        fileChooser = new JFileChooser();
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        JMenuBar menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("Archivo");
+        JMenuItem saveItem = new JMenuItem("Guardar");
+        JMenuItem saveAsItem = new JMenuItem("Guardar como");
+
+        saveItem.addActionListener(e -> saveFile());
+        saveAsItem.addActionListener(e -> saveFileAs());
+
+        fileMenu.add(saveItem);
+        fileMenu.add(saveAsItem);
+        menuBar.add(fileMenu);
+        setJMenuBar(menuBar);
+        add(scrollPane);
+    }
 }
 
 
